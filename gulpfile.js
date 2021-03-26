0ba96440c4754f2f3522b109ce380b074914168a
erikakasperaviciute <erika.kasperaviciute@hotmail.lt>
2021-03-27 00:34:44 +0200

    su skirtingais scss pradzia

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.task('serve', gulp.series(function(done) {
         }
     });
 
-    gulp.watch("scss/*.scss", gulp.series('sass'));
+    gulp.watch("scss/**/*.scss", gulp.series('sass'));
     gulp.watch("*.html").on('change', browserSync.reload);
 
     done();
